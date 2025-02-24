53e31c82961695988895d9839cecd1deb86da0c3
Daniilsol200 <daniilsol200@gmail.com>
2025-02-24 18:39:10 +0300

    Добавили классы и xsd схему

---
+++ EquipmentCatalogSolution/EquipmentCatalogLibrary/Equipment.cs
@@ -0,0 +1,10 @@
+﻿namespace EquipmentCatalogLibrary
+{
+    public class Equipment
+    {
+        public string Name { get; set; }
+        public string OwnerOrganization { get; set; }
+        public decimal Cost { get; set; }
+        public int ManufactureYear { get; set; }
+    }
+}
